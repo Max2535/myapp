d459c196a59651f087054962d44a66007fabb109
Max <kalmro4@gmail.com>
2025-12-05 17:17:07 +0700

    Add initial unit test for application sanity check

---
+++ src/test/java/com/example/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ApplicationTest {
+
+    @Test
+    void sanity() {
+        assertTrue(1 + 1 == 2);
+    }
+}
